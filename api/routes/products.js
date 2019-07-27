2e97f4008398e53a1d791f3391718f992d4f0308
root <root@localhost.localdomain>
2019-07-27 09:06:31 +0530

    Cloud MongoDB and Mongoose package

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,54 +1,102 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
+
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        messgae: "Handling GET requests to /products"
+    Product
+    .find()
+    .exec()
+    .then( doc =>{
+        console.log(doc)
+        res.status(200).json(doc)
+    })
+    .catch( err =>{
+        console.log(err)
+        res.status(500).json({error:err})
     });
 });
 
 router.post('/', (req, res, next) => {
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    }
-    res.status(201).json({
-        message: "Handling POST requests to /products",
-        products: product
+    })
+    product
+    .save()
+    .then(result => {
+        console.log(result)
+        res.status(201).json({
+            message: "Handling POST requests to /products",
+            products: product
+        });
+    })
+    .catch(err => {
+        console.log('error');
+        res.status(500).json({error: err });
     });
+    
 });
 
 // productID
 
 router.get('/:productID', (req, res, next) => {
-    
     const id = req.params.productID
-    if(id === 'special'){
-        res.status(200).json({
-            message: '(special) ID found.'
-        })
+    Product.findById(id)
+    .exec()
+    .then( doc =>{
+        console.log("from database", doc);
+        if(doc){
+            res.status(200).json(doc);
+        }else{
+            res.status(404).json({
+                message: 'Element Not Found'
+            });
+        }
         
-    }
-    else{
-        res.status(201).json({
-            message: 'You passed an ID.'
-        })
-            
-    }
+    })
+    .catch( error => {
+        console.log("error:"+error)
+        res.status(500).json({err:error})
+    })
+});
+
+
+router.patch('/:productID', (req, res, next) => {
+   const id = req.params.productID
+   const updateOps ={}
+   for(const ops of req.body){
+       updateOps[ops.propName]= ops.value;
+   }
+   Product
+   .update({ _id:id }, {$set:updateOps})
+   .exec()
+   .then( doc => {
+       console.log(doc)
+       res.status(200).json(doc)
+   })
+   .catch( err => {
+       console.log(err)
+       res.status(500).json({error:err})
+   })
     
 });
 
-
-router.patch('/', (req, res, next) => {
-   res.status(200).json({
-       message: 'Updated patch(product)'
-   });
-});
-
-router.delete('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted products'
-    });
+router.delete('/:productID', (req, res, next) => {
+    const id = req.params.productID
+    Product
+    .remove({ _id:id })
+    .then(doc => {
+        res.status(200).json({doc})
+        console.log(doc)
+    })
+    .catch( err => {
+        console.log(err)
+        res.status(500).json({error:err})
+    })
  });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express =  require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 const productsRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://admin:'+ process.env.MONGO_ATLAS_PWD +'@cluster0-xlx1c.mongodb.net/test?retryWrites=true&w=majority',
+{ useNewUrlParser: true });
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
